e64966fe31002e6857db641c1fbafb6c76204f5f
Lorenzo Polizzi <149718274+LolloPoli@users.noreply.github.com>
2024-11-28 15:57:49 +0100

    Update run_all.C

--- run_all.C
+++ run_all.C
@@ -1,5 +1,5 @@
 void run() {
-
+    /*
     main11();
     main12();
     main13();
@@ -7,38 +7,54 @@ void run() {
     main15();
     main16();
     main17();
-    std::cout << " " << std::endl;
-    std::cout << " --------------- 0.25 ------------------ " << std::endl;
-    std::cout << " " << std::endl;
     main18();
     main19();
     main20();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.25 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
     main21();
     main22();
     main23();
     main24();
     main25();
-    std::cout << " " << std::endl;
-    std::cout << " --------------- 0.50 ------------------ " << std::endl;
-    std::cout << " " << std::endl;
     main26();
     main27();
     main28();
     main29();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.50 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
     main30();
     main31();
     main32();
-    std::cout << " " << std::endl;
-    std::cout << " --------------- 0.75 ------------------ " << std::endl;
-    std::cout << " " << std::endl;
     main33();
     main34();
     main35();
     main36();
     main37();
     main38();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.75 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
     main39();
     main40();
+    main41();
+    main42();
+    main43();
+    main44();
+    main45();
+    main46();
+    main47();
+    main48();
+    */
+    main49();
+    main50();
+    main51();
+    main52();
+    main53();
+    main54();
+
 
     std::cout << "Tutti i main sono stati calcolati." << std::endl;
 }
